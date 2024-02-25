89230eaf832f4290131c7b73af85750597e90c0f
aryan-cs <aryan05g@gmail.com>
2024-02-25 10:03:51 -0600

    z-axis working!!

--- virtualization/unity-simulation/Assets/HandTracking.cs
+++ virtualization/unity-simulation/Assets/HandTracking.cs
@@ -30,11 +30,11 @@ public class HandTracking : MonoBehaviour
         float z0 = float.Parse(points[5 * 3 + 2]) - float.Parse(points[17 * 3 + 2]);
         float width_pix = Mathf.Sqrt(Mathf.Pow(x0, 2) + Mathf.Pow(y0, 2) + Mathf.Pow(z0, 2));
 
-        float z_hand = 0.05f * width_pix + 7.5f;
+        float z_hand = -0.5f * width_pix + 40.0f;
         float hand_size = 50 / width_pix;
 
-        wholeHand.transform.localPosition = new Vector3(0, 0, z_hand);
         wholeHand.transform.localScale = new Vector3(hand_size, hand_size, hand_size);
+        wholeHand.transform.localPosition = new Vector3(0, 0, z_hand);
 
         for (int i = 0; i < 21; i++)
         {
